2ce2ec098453c90829e6b0118945a29c41214551
Yeison <enmanuelquiterios@gmail.com>
2021-12-10 17:17:46 -0500

    Completed need to be review

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,25 @@
-// add code snippets from README
+let state = { count: 0 };
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+console.log(reducer({ count: 0 }, { type: "counter/increment" }));
+
+const dispatch = (action) => {
+  state = reducer(state, action);
+};
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
